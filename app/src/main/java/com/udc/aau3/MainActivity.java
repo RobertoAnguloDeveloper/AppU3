2b02cd080e81d089f56b5140cb240d96da85b0a8
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-12-27 21:48:37 -0500

    Contact book->OptionsMenu Created

--- app/src/main/java/com/udc/aau3/MainActivity.java
+++ app/src/main/java/com/udc/aau3/MainActivity.java
@@ -3,6 +3,9 @@ package com.udc.aau3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.menuItem1:
+                Toast.makeText(this, "MenuItem1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuItem2:
+                Toast.makeText(this, "MenuItem2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuItem3:
+                Toast.makeText(this, "MenuItem3", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuItem4:
+                Toast.makeText(this, "MenuItem4", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuItemSalir:
+                System.exit(0);
+                break;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
